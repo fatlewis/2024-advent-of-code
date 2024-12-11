e720cf129a767e29712808260d27a56c97725296
Matt Lewis <matt@m-lewis.co.uk>
2024-12-11 14:36:26 +0000

    day11 solution

---
+++ day-11.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"time"
+	"maps"
+)
+
+func toInt(str string) int {
+	result, _ := strconv.Atoi(str)
+	return result
+}
+
+func updateStone(stone int) []int {
+	if (stone == 0) {
+		return []int{1}
+	}
+	stoneString := strconv.Itoa(stone)
+	if (len(stoneString) % 2 == 0) {
+		return []int{
+			toInt(stoneString[0:len(stoneString)/2]),
+			toInt(stoneString[len(stoneString)/2:]),
+		}
+	}
+	// check for overflow? maybe use float?
+	return []int{stone * 2024}
+}
+
+func updateStones(stoneMap map[int]int) map[int]int {
+	newMap := make(map[int]int)
+	for stone := range maps.Keys(stoneMap) {
+		newStones := updateStone(stone)
+		for _, newStone := range newStones {
+			newMap[newStone] += stoneMap[stone]
+		}
+	}
+	return newMap
+}
+
+func sumValues(stoneMap map[int]int) int {
+	result := 0
+	for stoneValue := range maps.Values(stoneMap) {
+		result += stoneValue
+	}
+	return result
+}
+
+func main() {
+	dat, _ := os.ReadFile("./assets/day11-input.txt")
+	stoneStrings := strings.Split(strings.Trim(string(dat), "\n"), " ")
+
+	stoneMap := make(map[int]int)
+	for _, stoneString := range stoneStrings {
+		stoneInt := toInt(stoneString)
+		stoneMap[stoneInt] += 1
+	}
+	fmt.Println(stoneMap)
+
+	start := time.Now()
+	for i := 0; i < 25; i += 1 {
+		stoneMap = updateStones(stoneMap)
+	}
+	fmt.Println(sumValues(stoneMap))
+	fmt.Println(time.Since(start))
+
+	start = time.Now()
+	for i := 0; i < 75; i += 1 {
+		stoneMap = updateStones(stoneMap)
+	}
+	fmt.Println(sumValues(stoneMap))
+	fmt.Println(time.Since(start))
+}
+
